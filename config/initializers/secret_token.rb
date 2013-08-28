32da4cd8ba2bf169e795fb3f2fd804c5b86e9eb3
Adam Pash <adam.pash@gmail.com>
2013-08-28 16:41:16 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyOpenidProvider::Application.config.secret_token = '35fc5d3b2a913efcc8f038fe65946943b1b451cd6a9db63e9be3b8224eefa9bb083f8e7b68ace2d5bcdc07762d3257a149dd2441c6f7c78673988abdcf09b3cf'
